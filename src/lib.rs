16995b6647ccfdf5bf588a595997346d4735bca0
Jacob Bumbuna <88384674+bumbuna@users.noreply.github.com>
2024-09-26 21:08:31 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+mod onesies {
+    pub fn count(i: u32) -> u32 {
+        let mut a = i;
+        let mut count = 0;
+        while a > 0 {
+            if (a & 0x1) == 1 {
+                count+=1;
+            }
+            a >>= 1;
+        }
+        count
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::onesies as onesies;
+
+    #[test]
+    fn it_works() {
+        let result = onesies::count(1);
+        assert_eq!(result, 1);
+    }
+    
+    #[test]
+    fn it_fails() {
+        let result = onesies::count(2);
+        assert_ne!(result, 2);
+    }
+}
